bc3f12c5ec1ff7c421edd29555a90b1b1aaaf545
Unnati Mishra <68197005+unnati-m@users.noreply.github.com>
2021-10-04 12:24:35 +0530

    Create meta_char.js

---
+++ meta_char.js
@@ -0,0 +1,12 @@
+<html> 
+<head> 
+<title>JavaScript RegExp exec Method</title> 
+</head> 
+<body> 
+<script type="text/javascript"> 
+var str = "Javascript is an interesting scripting 
+language"; var re = new RegExp( "script", "g" ); 
+var result = re.test(str); 
+document.write("Test 1 - returned value : " + result); </script> 
+</body> 
+</html>
