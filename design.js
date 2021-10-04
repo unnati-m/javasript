0ccf23616bdf737f20f39b12f91d63f118c100fc
Unnati Mishra <68197005+unnati-m@users.noreply.github.com>
2021-10-04 08:37:11 +0530

    Create design.js

---
+++ design.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Welcome TO html.com</title>
+	<h1>news about html</h1>
+	
+</head>
+<body>
+	<h1>news about html</h1>
+	<h2>news bt html</h2> <!-->change to JS-->
+	<h3>news abt JS</h3><!-->change to JS-->
+	<h4>news abt html</h4>
+	<h5>news abt html</h5>
+	<h6>news abt html</h6>
+	<!-->this is my command line-->
+	<p><b><u>Encyclopaedia Britannica's</u> editors oversee subject areas</b> in which they have extensive knowledge, <i>whether from years of experience gained</i><a href="https://www.youtube.com/watch?v=eAzjv35y-hY&t=1004s">video</a></p>
+	<!--list-->
+	<ul>
+		<li>This is Unnati.</li>
+		<li>This is Riya</li>
+		<li>This is Danny</li>
+	</ul>
+	<ol>
+		<li>Unnati</li>
+		<li>Unnati</li>
+		<li>unnati</li>
+	</ol>
+
+</body>
+</html>
