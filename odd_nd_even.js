671408cf4345a47ea4ec0c77de2c39939262d0f8
Unnati Mishra <68197005+unnati-m@users.noreply.github.com>
2021-10-04 08:58:06 +0530

    Create odd_nd_even.js

---
+++ odd_nd_even.js
@@ -0,0 +1,17 @@
+<!doctype html> 
+<html> 
+<head> 
+<script> 
+function odd_even() 
+{ var no; no=Number(document.getElementById("no_input").value); if(no%2==0) 
+{ alert("Even Number"); 
+} 
+Else 
+{ alert("Odd Number"); } 
+} </script> 
+</head> 
+<body> 
+Enter Any Number:<input id="no_input"><br /> 
+<button onclick="odd_even()">Click me</button> 
+</body> 
+</html>
