cb4e867326c424436d27a55d489aa5cd5667e6f8
Unnati Mishra <68197005+unnati-m@users.noreply.github.com>
2021-10-04 08:57:17 +0530

    Create add_2_num.js

---
+++ add_2_num.js
@@ -0,0 +1,14 @@
+<html> 
+<body> 
+<script> 
+const num1 = 5;  
+const num2 = 3; 
+// add two numbers 
+const sum = num1 + num2;  
+alert(sum); 
+document.write(sum) 
+// display the sum 
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum); 
+</script> 
+</body> 
+</html>
